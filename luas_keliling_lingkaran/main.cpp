6be861089dbb4f7b0a1e9e83b7df4f282383446b
Dara Nailah Ikhsyan <125743006+daraxnailah@users.noreply.github.com>
2025-08-15 13:47:43 +0800

    Add files via upload

---
+++ luas_keliling_lingkaran/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+float luas_lingkaran(int r) {
+    float phi = 3.14, luas;
+    luas = phi * r * r;
+    return luas;
+}
+
+float keliling_lingkaran(int r) {
+    float phi = 3.14, keliling;
+    keliling = 2 * phi * r;
+    return keliling;
+}
+
+int main() {
+    cout << "Menghitung Luas & Keliling Lingkaran\n";
+    int r;
+    cout << "Masukan Jari-jari : ";
+    cin >> r;
+    cout << "Luas Lingkaran    = " << luas_lingkaran(r) << endl;
+    cout << "Keliling Lingkaran= " << keliling_lingkaran(r) << endl;
+}
